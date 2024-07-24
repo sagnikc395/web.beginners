8fcb61e1f05e4cbb384f270e172e23e1f3d50f03
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-07-24 18:46:27 +0530

    src: basic code practice

--- src/week1.2/basics.js
+++ src/week1.2/basics.js
@@ -3,3 +3,28 @@
 console.log("hello everynion! 😸");
 let a = "muhahahahah!";
 console.log(a);
+
+function greetPerson(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}! Welcome to some programming fun!`;
+}
+
+console.log(greetPerson("sagnik", "chatterjee"));
+
+//greet on gender
+function greetOnGender(firstName, lastName, gender) {
+  if (gender.toLowerCase() === "male") {
+    return `Hello Mr. ${firstName} ${lastName} ! Welcome here!`;
+  } else if (gender.toLowerCase() === "female") {
+    return `Hello Ms. ${firstName} ${lastName}! Welcome here!`;
+  } else {
+    return `Sir this is a conservative state! 😭😈🔫`;
+  }
+}
+
+console.log(greetOnGender("Sherlock", "Holmes", "trans"));
+
+//counts from 0-1000 and prints
+for (let i = 0; i < 1001; i++) {
+  console.log(`Item number ${i}\t`);
+}
+
