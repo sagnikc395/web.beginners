70c6eb0cf0bf378f47b95ddd7b72c525e834ee29
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-07-25 01:35:45 +0530

    src: bunch of math util funcs in js

---
+++ src/week1.3/math-practice.js
@@ -0,0 +1,15 @@
+//bunch of high level maths things, which are used mostly
+
+console.log(Math.round(3.14159));
+console.log(Math.ceil(123.2));
+console.log(Math.floor(123.987));
+
+console.log(Math.random());
+
+console.log(Math.max([1, 123, 13, 123123]));
+console.log(Math.max(1, 12, 123, 123));
+
+console.log(Math.min([123123, 123323, 1231212313]));
+
+console.log(Math.pow(12, 2));
+console.log(Math.sqrt(14));
