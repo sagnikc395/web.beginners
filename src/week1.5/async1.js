1c8650cbe2eae309900e559e7cb15d07b6250967
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-07-25 08:45:33 +0530

    src: async and sync understanding

--- src/week1.5/async1.js
+++ src/week1.5/async1.js
@@ -10,8 +10,26 @@ function findSumTill100() {
   console.log(findSum(100));
 }
 
+let start = new Date().getTime();
 //making this sync using busy waiting
-function syncSleep() {}
+//thread isnt doing anything , its just synchronously very busy here
+function syncSleep() {
+  let a = 1;
+  for (let i = 0; i < 1000_000_000; i++) {
+    a++;
+  }
+}
 
-setTimeout(findSumTill100, 1 * 1000);
-console.log("All Done");
+syncSleep();
+let end = new Date().getTime();
+// setTimeout(findSumTill100, 1 * 1000);
+console.log("Sync Func Done");
+console.log(`Time Taken in ms : ${end - start}`);
+
+start = new Date().getTime();
+//calling our async func
+setTimeout(syncSleep, 2 * 1000);
+console.log(`Async Func called`);
+end = new Date().getTime();
+
+console.log(`Time taken after invoking async func ms: ${end - start}`);
