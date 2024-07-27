f18dcb1b88b3318e64ef18d56f5eaaf52c7bab24
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-07-27 15:48:19 +0530

    src: understanding functional programming in js

---
+++ docs/week2.6.md
@@ -0,0 +1,6 @@
+## map,filter and arrow fns
+
+- map and filter are helper functions for arrays
+- arrow fns are another way to write functions in js.
+- in arrow fns and normal fns there is a difference in how `this` gets binded in the function scope.
+
---
+++ src/week2.6/functional-programming.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => a + b;
+
+const ans = sum(1, 2);
+console.log(ans);
+
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newArray = input.filter((item) => item % 2 == 0);
+console.log(newArray);
+
+const newArr2 = input.map((item) => item * 2);
+console.log(newArr2);
+
